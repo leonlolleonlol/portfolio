c854dcb314fe50ebc08c10bd1094a687dfc841fe
leonlolleonlol <leonkoj2004@gmail.com>
2024-05-04 01:08:50 -0400

    section 2 step 7

---
+++ dist/index.b4ce7916.js
@@ -0,0 +1,16 @@
+const bar = document.querySelector(".loading__bar--inner");
+const counter_num = document.querySelector(".loading__counter--number");
+let c = 0;
+let barInterval = setInterval(()=>{
+    bar.style.width = c + "%";
+    counter_num.innerText = c + "%";
+    c++;
+    if (c === 101) {
+        clearInterval(barInterval);
+        document.querySelector(".loading").classList.add("loading--hidden");
+        document.getElementById("eye").style.background = "#d9ff88";
+        document.getElementById("eye2").style.background = "#d9ff88";
+    }
+}, 20);
+
+//# sourceMappingURL=index.b4ce7916.js.map
