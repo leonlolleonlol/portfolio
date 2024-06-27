157c446506270922528e056c9cd2a72682114537
leonlolleonlol <leonkoj2004@gmail.com>
2024-06-26 20:48:58 -0400

    added path section

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -3,6 +3,7 @@ import '../styles/coolButton.scss';
 import '../styles/face.scss';
 import '../styles/landing.scss';
 import '../styles/loader.scss';
+import '../styles/path.scss';
 import '../styles/projects.scss';
 import '../styles/skills.scss';
 
